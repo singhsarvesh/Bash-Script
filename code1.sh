68ca8a05de79b9e14b661504095b729e87c9ecf2
Sarvesh <sarveshsingh.inc@gmail.com>
2024-12-18 01:04:29 -0500

    m2

---
+++ code1.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#lets start
+
+echo "Enter number"
+
+read -p "Enter the first number: " num1
+read -p "Enter the second number: " num2
+
+result=$((num1 + num2))
+
+echo "Result: $num1 + $num2 = $result"
